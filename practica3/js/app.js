2e07e39c16f5127667cf13b0689a5fb4ebe0dce9
maferlzm <maferlzm@gmail.com>
2020-02-12 09:27:46 -0600

    Calcular Ã¡rea de un triÃ¡ngulo

---
+++ practica3/js/app.js
@@ -0,0 +1,19 @@
+/**Dada la base y la altura de un triangulo, realizar el pseudocodigo que escriba el área del triángulo */
+
+let altura;
+let base;
+let area;
+
+let button=document.getElementById('btnCalcular');
+
+button.addEventListener('click', calcularArea);
+
+function calcularArea(){
+    altura= Number(document.getElementById('altura').value);
+    base= Number(document.getElementById('base').value);
+
+    area=(base*altura)/2;
+
+    let calculo=document.getElementById('calculo');
+    calculo.innerText=area;
+}
