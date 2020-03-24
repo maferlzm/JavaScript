709c8dc6c06e2b0bb4c3db21fb21f233c5cd1103
maferlzm <maferlzm@gmail.com>
2020-03-23 21:47:31 -0600

    Arrays - Arreglo invertido

---
+++ Arrays/practica10/js/index.js
@@ -0,0 +1,31 @@
+/**Llenar con una palabra un arreglo y regresar 
+ * un arreglo que muestre el primero, pero invertido. */
+
+EventListener();
+
+function EventListener() {
+    document.getElementById('arrayP').addEventListener('keypress', InitApp);
+}
+
+function InitApp() {
+    if (event.key !== 'Enter') return;
+    if (event.target.value == '') return;
+
+    arrayInvertido(event.target.value);
+}
+
+function arrayInvertido(stringArray) {
+    let palabra = [];
+    palabra = stringArray.split('');
+    let palabraInvertida = palabra.reverse().join('').toString();
+
+    console.log(palabraInvertida);
+
+    const output = document.getElementById('output');
+    const div = document.createElement('div');
+    div.setAttribute('class', 'alert alert-success mt-3');
+    div.innerText = palabraInvertida;
+
+    output.appendChild(div);
+
+}
